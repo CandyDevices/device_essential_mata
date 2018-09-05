ProtonAOSP/android_device_google_wahoo

Power: Implement PowerHal 1.2
Convert all perfd hints into PowerHAL hints

Test: do camera/audio powerhint
Bug: 62041945
Change-Id: I82c8ca99b76d70d716eabedb617a126446646b7d

device.mk
@@ -170,7 +170,7 @@ PRODUCT_COPY_FILES += \
 
 # power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.wahoo
+    android.hardware.power@1.2-service.wahoo
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
@@ -611,7 +611,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Enable CameraHAL perfd usage
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.perfd.enable=true
+    persist.camera.perfd.enable=false
 
 # Enable Gcam FD Ensemble
 PRODUCT_PROPERTY_OVERRIDES += \

CandyDevices/device_essential_mata

Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

device.mk
@@ -316,7 +316,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.mata
+    android.hardware.power@1.2-service.mata
 
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml