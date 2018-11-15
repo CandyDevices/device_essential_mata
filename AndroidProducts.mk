87b633da3b649666f5b7722c8c6d3a062f2e5e99
kevin princiotta <kevinprinciotta@gmail.com>
2018-11-15 12:33:50 -0500

    Candify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 The LineageOS Project
+# Copyright (C) 2017-19 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES += \
-    device/essential/mata/lineage_mata.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/candy_mata.mk
--- candy_mata.mk
+++ candy_mata.mk
@@ -1,6 +1,6 @@
-# Inherit some common Lineage stuff.
+    # Inherit some common Candy stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -20,7 +20,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := candy_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
@@ -28,6 +28,6 @@ PRODUCT_RELEASE_NAME := mata
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=mata \
-        PRIVATE_BUILD_DESC="mata-user 9 PQ1A.190105.024 462 release-keys"
+        PRIVATE_BUILD_DESC="mata-user 9 PPR1.181005.076 390 release-keys"
 
-BUILD_FINGERPRINT := essential/mata/mata:9/PQ1A.190105.024/462:user/release-keys
+BUILD_FINGERPRINT := essential/mata/mata:9/PPR1.181005.076/390:user/release-keys
