7446cf91744c1f38ddab8487d1fa90678fe0fe6d
kevin princiotta <kevinprinciotta@gmail.com>
2018-12-24 16:54:52 -0500

    Switch to Opengapps

--- device.mk
+++ device.mk
@@ -159,6 +159,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/essential/mata/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
+# GApps
+GAPPS_VARIANT := nano
+GAPPS_FORCE_WEBVIEW_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_DIALER_OVERRIDES := true
+GAPPS_FORCE_BROWSER_OVERRIDES := true
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
