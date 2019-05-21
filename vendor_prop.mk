166bb018b082dd43b03ccbf2b30adc4fdc1c3380
kevin princiotta <kevinprinciotta@gmail.com>
2019-05-21 15:09:08 -0400

    mata:Add IMS vendor props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -64,6 +64,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.hdr.config=/vendor/etc/hdr_tm_config.xml
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.dbg.ims_volte_enable=1
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.cne.feature=1 \
