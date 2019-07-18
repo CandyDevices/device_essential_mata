9ce1c63d03836a7b13518afa5b3e50ce6555b422
celtare21 <celtare21@gmail.com>
2019-07-18 14:53:38 +0000

    mata: Order Overlay better
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- device.mk
+++ device.mk
@@ -248,6 +248,10 @@ PRODUCT_COPY_FILES += \
     device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     device/essential/mata/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Overlay
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
