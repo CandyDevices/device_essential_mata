1bf214425d4469d34d05b1f86cf24082dacd9652
hiper25 <2243168976@qq.com>
2021-01-15 20:27:21 +0800

    mata: DeviceSettings: Remove strings and track them from our org

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -15,14 +15,13 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 include frameworks/base/packages/SettingsLib/common.mk
 
