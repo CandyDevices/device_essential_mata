85da5eaca8b70596a788e80a6e699227900e1c5f
Kevin <kevinprinciotta@gmail.com>
2021-05-20 15:05:06 -0400

    Candify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES += \
-    device/essential/mata/aosp_mata.mk
+    device/essential/mata/candy_mata.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mata-user \
-    aosp_mata-userdebug \
-    aosp_mata-eng
+    candy_mata-user \
+    candy_mata-userdebug \
+    candy_mata-eng
